e7cd48d022258d0b79b7857d654b36ac104100f5
luanjiahao <2176581937@qq.com>
2025-05-28 22:42:55 +0800

    中州养老后端初始代码

---
+++ zzyl-nursing-platform/src/main/java/com/zzyl/serve/mapper/NursingPlanMapper.java
@@ -0,0 +1,61 @@
+package com.zzyl.serve.mapper;
+
+import java.util.List;
+import com.zzyl.serve.domain.NursingPlan;
+
+/**
+ * 护理计划Mapper接口
+ * 
+ * @author ruoyi
+ * @date 2025-05-27
+ */
+public interface NursingPlanMapper 
+{
+    /**
+     * 查询护理计划
+     * 
+     * @param id 护理计划主键
+     * @return 护理计划
+     */
+    public NursingPlan selectNursingPlanById(Integer id);
+
+    /**
+     * 查询护理计划列表
+     * 
+     * @param nursingPlan 护理计划
+     * @return 护理计划集合
+     */
+    public List<NursingPlan> selectNursingPlanList(NursingPlan nursingPlan);
+
+    /**
+     * 新增护理计划
+     * 
+     * @param nursingPlan 护理计划
+     * @return 结果
+     */
+    public int insertNursingPlan(NursingPlan nursingPlan);
+
+    /**
+     * 修改护理计划
+     * 
+     * @param nursingPlan 护理计划
+     * @return 结果
+     */
+    public int updateNursingPlan(NursingPlan nursingPlan);
+
+    /**
+     * 删除护理计划
+     * 
+     * @param id 护理计划主键
+     * @return 结果
+     */
+    public int deleteNursingPlanById(Integer id);
+
+    /**
+     * 批量删除护理计划
+     * 
+     * @param ids 需要删除的数据主键集合
+     * @return 结果
+     */
+    public int deleteNursingPlanByIds(Integer[] ids);
+}
